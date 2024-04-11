8b8cf4599d283e69c2524c05d250780c084dd66f
malotaibi44 <152829779+malotaibi44@users.noreply.github.com>
2024-04-11 18:05:47 -0400

    Update pitchmoving.m

--- pitchmoving.m
+++ pitchmoving.m
@@ -1,5 +1,4 @@
-
-function [pitch_shifted_signal] = psola(sp)
+function [pitch_shifted_signal] = psola(sp,pitch_period,new_pitch_period)
 
 % [sp , Fs] = audioread("parham.wav");
 % %sp=sp(2293:2293+441);
@@ -7,37 +6,40 @@ function [pitch_shifted_signal] = psola(sp)
 % %Establish upper and lower pitch search limits
 % pmin=30; pmax=200;
 % sp2=sp.^2;% pre-calculate to save time
-% for M=pmin:pmax
-%     e_del=sp(1:n-M);
-%     e=sp(M+1:n);
-%     e2=sp2(M+1:n);
-%     E(1+M-pmin)=sum((e_del.*e).^2)/sum(e2);
+% for pitch_period=pmin:pmax
+%     e_del=sp(1:n-pitch_period);
+%     e=sp(pitch_period+1:n);
+%     e2=sp2(pitch_period+1:n);
+%     E(1+pitch_period-pmin)=sum((e_del.*e).^2)/sum(e2);
 % end
-% %Find M, the optimum pitch period
-% [i, M]=max(E);
-% M=M+pmin;
+% %Find pitch_period, the optimum pitch period
+% [i, pitch_period]=max(E);
+% pitch_period=pitch_period+pmin;
 % %Find B, the pitch gain
-% e_del=sp(1:n-M);
-% e=sp(M+1:n);
-% e2=sp2(M+1:n);
+% e_del=sp(1:n-pitch_period);
+% e=sp(pitch_period+1:n);
+% e2=sp2(pitch_period+1:n);
 % B=sum(e_del.*e)/sum(e2);
  
 %%
-[M,B]=pitch_estimation_Long_term(sp);
-sc=2;
-M2=round(M*sc);
- 
-win=hamming(1,2*M);
-N=floor(length(sp)/M);
-pitch_shifted_signal=zeros(N*M2+M,1);
+%
+% according to the specs of the problem, if the new pitch period is more
+% than 10 then it is multiplied to the original pitch (speeding the 
+M2=new_pitch_period;
+if new_pitch_period<10
+    M2=round(pitch_period*new_pitch_period);
+end
+win=hamming(1,2*pitch_period);
+N=floor(length(sp)/pitch_period);
+pitch_shifted_signal=zeros(N*M2+pitch_period,1);
 for n = 1:N-1 
-    fr1=1+(n-1)*M;
-    to1=n*M+M;
+    fr1=1+(n-1)*pitch_period;
+    to1=n*pitch_period+pitch_period;
     seg=sp(fr1:to1).*win;
-    fr2=1+(n-1)*M2-M;
-    to2=(n-1)*M2+M;
+    fr2=1+(n-1)*M2-pitch_period;
+    to2=(n-1)*M2+pitch_period;
     fr2b=max([1,fr2]);
-    pitch_shifted_signal(fr2b:to2)=pitch_shifted_signal(fr2b:to2)+seg(1+fr2b-fr2:2*M);
+    pitch_shifted_signal(fr2b:to2)=pitch_shifted_signal(fr2b:to2)+seg(1+fr2b-fr2:2*pitch_period);
 end
  
 % figure()
